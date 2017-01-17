b7e1fab316cadfe6ac202f2e6957c4f311dead7b
Martijn Barendregt <barendregt@fastmail.com>
2017-01-17 13:09:20 +0100

    x

---
+++ run_Expectation_Mac.py
@@ -0,0 +1,17 @@
+import sys, datetime
+# from Tkinter import *
+
+from IPython import embed
+
+sys.path.append( 'exp_tools' )
+
+from ExpectationSession import *
+
+# useTracker = True
+
+inScanner =0 
+useTracker =0
+expTask = 1
+
+ts = ExpectationSession( sid, 0, inScanner, useTracker, expTask )
+ts.run()		
